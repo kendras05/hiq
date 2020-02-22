390fc789a699e7fe703e2a98121f3f1d079547ce
Justin Mitchell <jmitchell@Justins-MacBook-Pro.local>
2020-02-21 22:31:28 -0800

    cleanup & app structure

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/app.css'
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to Week 15 LAB
+    </div>
+  );
+}
+
+export default App;
